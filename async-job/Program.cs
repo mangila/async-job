6a59aa04daf11f3d24555309ff1cd5eee70a0b3d
Erik Olsson <olsson.erik1993@gmail.com>
2025-09-10 22:39:37 +0200

    remove bloat

--- async-job/Program.cs
+++ async-job/Program.cs
@@ -4,10 +4,6 @@ using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 builder.Services.AddSingleton<ITaskQueue, DefaultTaskQueue>();
 builder.Services.AddHostedService<Worker>();
 // Configure Redis
@@ -21,20 +17,19 @@ builder.Services.AddStackExchangeRedisCache(redisOptions =>
 var app = builder.Build();
 
 app.MapPost("/start-job",
-        async (HttpContext ctx, ITaskQueue queue, IDistributedCache redis, ILogger<Program> logger) =>
+    async (HttpContext ctx, ITaskQueue queue, IDistributedCache redis, ILogger<Program> logger) =>
+    {
+        var guid = Guid.NewGuid();
+        redis.SetString(guid.ToString(), "PENDING");
+        await queue.EnqueueAsync(async ct =>
         {
-            var guid = Guid.NewGuid();
-            redis.SetString(guid.ToString(), "PENDING");
-            await queue.EnqueueAsync(async ct =>
-            {
-                await redis.SetStringAsync(guid.ToString(), "STARTING", ct);
-                // simulate task execution
-                await Task.Delay(TimeSpan.FromSeconds(30), ct);
-                await redis.SetStringAsync(guid.ToString(), "COMPLETE", ct);
-            });
-            return guid;
-        })
-    .WithName("StartJob");
+            await redis.SetStringAsync(guid.ToString(), "STARTING", ct);
+            // simulate task execution
+            await Task.Delay(TimeSpan.FromSeconds(30), ct);
+            await redis.SetStringAsync(guid.ToString(), "COMPLETE", ct);
+        });
+        return guid;
+    });
 
 app.MapGet("/status-job/{guid}",
     (string guid, IDistributedCache redis, ILogger<Program> logger) => redis.GetString(guid));
